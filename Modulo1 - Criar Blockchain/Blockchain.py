9bc26a4d939c959d0692b9d9019f432968621bec
Thiago <thiagoworkspace@outlook.com>
2020-03-26 18:45:06 -0300

    .

---
+++ Modulo1 - Criar Blockchain/Blockchain.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 18:32:44 2020
+
+@author: thiago
+"""
+
+"""
+Importar a liby dateTime 
+pq cada bloco quando for criado vai ter sua data exata
+
+Importar a liby hashlib
+pq vai ser produzido hashs
+
+import json
+vai codificar os blocos com json antes e depois gerar os hashs
+
+from flask import flask, jsonify -> para produzir arquivos em formato json
+"""
+
+import datetime
+import hashlib
+import json
+from flask import flask, jsonify
+
+
+
